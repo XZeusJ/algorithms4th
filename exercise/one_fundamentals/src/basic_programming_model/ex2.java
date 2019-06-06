dc6588393fbe90ff24fc29bccff5c6349eeab6de
XzJ <394386241@qq.com>
2019-06-06 14:24:07 +0800

    initial data and exercise

---
+++ exercise/one_fundamentals/src/basic_programming_model/ex2.java
@@ -0,0 +1,10 @@
+package basic_programming_model;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class ex2 {
+    public static void main(String[] args) {
+        StdOut.println((1+2.236)/2);
+        StdOut.println(1 + 2 + 3 + 4.0);
+    }
+}
