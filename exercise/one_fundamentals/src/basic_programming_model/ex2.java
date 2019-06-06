cb0d9be77a6cba7356cd0c32c137b167ae7ccc53
XzJ <394386241@qq.com>
2019-06-06 16:53:58 +0800

    update basic code

--- exercise/one_fundamentals/src/basic_programming_model/ex2.java
+++ exercise/one_fundamentals/src/basic_programming_model/ex2.java
@@ -6,5 +6,7 @@ public class ex2 {
     public static void main(String[] args) {
         StdOut.println((1+2.236)/2);
         StdOut.println(1 + 2 + 3 + 4.0);
+        StdOut.println(4.1 >= 4);
+        StdOut.println(1 + 2+ "3");
     }
 }
---
+++ exercise/one_fundamentals/src/basic_programming_model/ex9.java
@@ -0,0 +1,18 @@
+package basic_programming_model;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ex9 {
+    public static void main(String[] args){
+        String s = "";
+        int N = StdIn.readInt();
+
+        for (int n = N; 0 < n; n /=2) {
+            s = (n % 2) + s;
+            StdOut.println("n is: "+n);
+            StdOut.println("s is: "+s);
+        }
+        StdOut.println(s);
+    }
+}
