933a6932ab35867c01b69b0f764e1e5c00cc1a2d
XzJ <394386241@qq.com>
2019-06-08 13:40:05 +0800

    del some files

--- exercise/one_fundamentals/src/basic_programming_model/ex11.java
+++ exercise/one_fundamentals/src/basic_programming_model/ex11.java
@@ -1,4 +1,31 @@
 package basic_programming_model;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
 public class ex11 {
+    public static void main(String[] args){
+        boolean[][] a ={ { true, false, true, true },
+                { false, true, false,true },
+                { true, true, false, true } };
+
+        int M = a.length;
+        int N = a[0].length;
+
+        // print col num
+        StdOut.print(" ");
+        for (int i = 0; i < N; i++) {
+            StdOut.print(i);
+        }
+        StdOut.println();
+
+        // print row num And a[i][j]
+        for (int i = 0; i < M; i++) {
+            StdOut.print(i);
+            for (int j = 0; j < N; j++) {
+                StdOut.print(a[i][j] ? "*" : ' ');
+            }
+            StdOut.println();
+        }
+    }
 }
--- exercise/one_fundamentals/src/basic_programming_model/ex13.java
+++ exercise/one_fundamentals/src/basic_programming_model/ex13.java
@@ -1,4 +1,54 @@
 package basic_programming_model;
 
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Random;
+
 public class ex13 {
+    public static void main(String[] args){
+        int M = 4;
+        int N = 6;
+        int[][] a = genArray(M, N);
+        printArray(a);
+
+        int[][] transA = transArray(a);
+        printArray(transA);
+    }
+
+    private static int[][] genArray(int M, int N){
+        Random random = new Random();
+        int[][] a = new int[M][N];
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                a[i][j] = random.nextInt(10) + 1;
+            }
+        }
+        return a;
+    }
+
+    private static void printArray(int[][] a){
+        int M = a.length;
+        int N = a[0].length;
+        for (int i = 0; i < M; i++) {
+            StdOut.println();
+            for (int j = 0; j < N; j++) {
+                StdOut.print(a[i][j]+" ");
+            }
+        }
+        StdOut.println();
+    }
+
+    private static int[][] transArray(int[][] a){
+        int M = a.length;
+        int N = a[0].length;
+        int[][] transA = new int[N][M];
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                transA[j][i] = a[i][j];
+            }
+        }
+        return transA;
+    }
 }
