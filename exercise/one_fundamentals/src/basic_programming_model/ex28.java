7d06c30e010dd2e687b44984b7843e5879a12c91
XzJ <394386241@qq.com>
2019-06-15 18:42:59 +0800

    update files

---
+++ exercise/one_fundamentals/src/basic_programming_model/ex28.java
@@ -0,0 +1,33 @@
+package basic_programming_model;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import java.util.Arrays;
+
+public class ex28 {
+
+    public static void main(String[] args) {
+        int[] whitelist = new In(args[0]).readAllInts();
+        Arrays.sort(whitelist);
+        int len = whitelist.length;
+
+        for (int i = 0; i < len; i++) {
+            StdOut.print(whitelist[i] + " ");
+        }
+        StdOut.println();
+
+        // set the repeat element into 0 in the whitelist
+        for (int i = 0; i < len - 1; i++) {
+            if (whitelist[i] == whitelist[i + 1]) whitelist[i] = 0;
+        }
+
+        Arrays.sort(whitelist);
+        for (int i = 0; i < len; i++) {
+            if (whitelist[i] != 0) {
+                StdOut.print(whitelist[i] + " ");
+            }
+        }
+
+
+    }
+}
