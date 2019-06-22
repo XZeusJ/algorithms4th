d941eef849ee2a734b9ba03b0d8185fec72e4bd7
XzJ <394386241@qq.com>
2019-06-22 21:18:40 +0800

    update files

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -17,21 +17,21 @@ public class Percolation {
     public Percolation(int n) { // create n-by-n grid, with all sites blocked
         gridSize = n;
         uf = new WeightedQuickUnionUF(
-                n * n); // record information about whether the sites is connected
-        sites = new int[n][n]; // record information about Open or block sites.
+                n * n + 1); // record information about whether the sites is connected
+        sites = new int[n + 1][n + 1]; // record information about Open or block sites.
         numOfOpenSites = 0;
     }
 
     private int xyTo1D(int x, int y) {
-        return x * gridSize + y; // map "sites" array entries to "uf" array entries
+        return gridSize * (x - 1) + y; // map "sites" array entries to "uf" array entries
     }
 
     // validate that p is a valid index
     private void validate(int row, int col) {
-        if (row < 0 || row >= gridSize) throw new IllegalArgumentException(
-                "index " + row + " is not between 0 and " + (gridSize - 1));
-        if (col < 0 || col >= gridSize) throw new IllegalArgumentException(
-                "index " + col + " is not between 0 and " + (gridSize - 1));
+        if (row <= 0 || row > gridSize) throw new IllegalArgumentException(
+                "index " + row + " is not between 1 and " + gridSize);
+        if (col <= 0 || col > gridSize) throw new IllegalArgumentException(
+                "index " + col + " is not between 1 and " + gridSize);
     }
 
     public void open(int row, int col) { // open site (row, col) if it is not open already
@@ -50,15 +50,14 @@ public class Percolation {
     }
 
     public boolean isOpen(int row, int col) { // is site (row, col) open?
-        if (row < 0 || row >= gridSize || col < 0 || col >= gridSize) return false;
+        if (row <= 0 || row > gridSize || col <= 0 || col > gridSize) return false;
         return sites[row][col] == 1;
     }
 
     public boolean isFull(int row, int col) { // is site (row, col) full?
         int q = xyTo1D(row, col);
-        for (int i = 0; i < gridSize; i++) {
-            // StdOut.println(i + " " + q + " is " + uf.connected(i, q));
-            if (uf.connected(i, q) && isOpen(row, col)) return true;
+        for (int j = 1; j <= gridSize; j++) {
+            if (uf.connected(j, q) && isOpen(row, col)) return true;
         }
         return false;
     }
@@ -68,19 +67,29 @@ public class Percolation {
     }
 
     public boolean percolates() { // does the system percolate?
-        for (int col = 0; col < gridSize; col++) {
-            if (isFull(gridSize - 1, col)) return true;
+        for (int col = 1; col <= gridSize; col++) {
+            if (isFull(gridSize, col)) return true;
         }
         return false;
     }
 
     public void showSites() {
-        for (int i = 0; i < sites.length; i++) {
-            for (int j = 0; j < sites[0].length; j++)
+        for (int i = 1; i <= gridSize; i++) {
+            for (int j = 1; j <= gridSize; j++)
                 StdOut.print(sites[i][j] + " ");
             StdOut.println();
         }
+        showFullSites();
         StdOut.println("is percolates? " + percolates());
+        StdOut.println();
+    }
+
+    public void showFullSites() {
+        for (int i = 1; i <= gridSize; i++) {
+            for (int j = 1; j <= gridSize; j++) {
+                if (isFull(i, j)) StdOut.printf("(%d, %d) is full.\n", i, j);
+            }
+        }
         StdOut.println();
     }
 
@@ -102,18 +111,20 @@ public class Percolation {
         // perc.showSites();
 
         // test isFull()
-        StdOut.println("0,2 is full? " + perc.isFull(0, 2));
-        StdOut.println("1,2 is full? " + perc.isFull(1, 2));
-        perc.open(0, 1);
-        StdOut.println("1,2 is full? " + perc.isFull(1, 2));
+        StdOut.println("(1,2) is full? " + perc.isFull(1, 2));
+        StdOut.println("(2,2) is full? " + perc.isFull(2, 2));
+        perc.open(2, 2);
+        StdOut.println("(2,2) is full after open? " + perc.isFull(2, 2));
+        perc.open(3, 3);
+        StdOut.println("(3,3) is full after open(3,3)? " + perc.isFull(3, 3));
+        perc.open(3, 2);
+        StdOut.println("(3,3) is full after open(3,2)? " + perc.isFull(3, 3));
 
+        StdOut.println();
 
         // test percolates()
-        // perc.open(0, 1);
-        // perc.showSites();
-        //
-        // perc.open(2, 1);
-        // perc.showSites();
+        perc.showSites();
+
 
     }
 }
---
+++ percolation/PercolationStats.java
@@ -0,0 +1,38 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class PercolationStats {
+    private int gridSize;
+    private int numTrials;
+
+    public PercolationStats(int n, int trials) {
+        gridSize = n;
+        numTrials = trials;
+
+
+    }
+
+    public double mean() {
+        return 0;
+    }
+
+    public double stddev() {
+        return 0;
+    }
+
+    public double confidenceLo() {
+        return 0;
+    }
+
+    public double confidenceHi() {
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
