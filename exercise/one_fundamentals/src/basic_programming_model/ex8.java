f40608b3507201d2853bc0df5b5688982d1b3313
XzJ <394386241@qq.com>
2019-07-01 20:07:52 +0800

    update files

--- exercise/one_fundamentals/src/basic_programming_model/ex8.java
+++
@@ -1,9 +0,0 @@
-package basic_programming_model;
-
-public class ex8 {
-    public static void main(String[] args) {
-        System.out.println('b');
-        System.out.println('b'+'c');
-        System.out.println((char)('a'+4));
-    }
-}
